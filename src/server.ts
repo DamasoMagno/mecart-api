3b25b034ed8d0e57f7b0a60985af40b1053c3ec7
DamasoMagno <limamdamaso@gmail.com>
2023-09-27 00:55:41 -0300

    refactor: Cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT;
 
@@ -6,6 +7,11 @@ import { cartRoute } from "./routes/cart.route";
 import { userRoute } from "./routes/user.route";
 import { productRoute } from "./routes/product.route";
 
+const corsOptions = {
+  origin: "https://mecart.onrender.com"
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/cart", cartRoute);
